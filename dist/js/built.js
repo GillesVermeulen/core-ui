c9890c699c52ae436b50f0d5d98e5ccf53d69e62
Rui Molefas <molefas@gmail.com>
2015-10-20 18:08:34 +0100

    Added platform base style

--- dist/js/built.js
+++ dist/js/built.js
@@ -20,6 +20,61 @@ $('.gallery-item').hover(function(){
 	$(this).removeClass('active');
 });
 
+//tabbed-pannels
+$('.mv-tab-panel .mv-side-panel-tabs li a').click(function() {
+
+	var $tabPanel = $(this).closest('.mv-tab-panel'),
+		tab = $(this).data('tab'),
+		$content = $tabPanel.find('.mv-tab-content[data-panel='+tab+']');
+
+	if ($content.length) {
+
+		// Toggle contents
+		$tabPanel.find('.mv-tab-content').removeClass('active');
+		$content.addClass('active');
+
+		// Toggle tabs active state
+		$tabPanel.find('.mv-side-panel-tabs li a').removeClass('active');
+		$(this).addClass('active');
+	}
+});
+
+// slider
+$('.mv-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow: '<i class="fa fa-angle-left nav-button nav-left"></i>',
+  nextArrow: '<i class="fa fa-angle-right nav-button nav-right"></i>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 // Masonry
 var $grid = $('.grid').masonry({
   	columnWidth: '.spacer',
